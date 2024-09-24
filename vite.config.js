2d271a842388d61d5e00c76b3c9e373273ff6538
Chris Chapman <chris@clickk.com.au>
2024-09-24 21:35:16 +1000

    cc

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import reactCssModules from '@vitejs/plugin-react-css-modules';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), reactCssModules()],
   build: {
     rollupOptions: {
-      external: ['mapbox-gl']
+      external: ['mapbox-gl', '@mapbox/gl']
     }
   }
 })
